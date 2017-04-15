cde23fae02915dd1d08678847847b6c50e208f77
kritzware <suspiciouspotato@gmail.com>
2017-04-16 00:21:40 +0100

    Added main renderer

---
+++ src/renderer.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const { dialog } = require('electron').remote
+
+require.extensions['.html'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8')
+}
+
+document.getElementById('main-app').innerHTML = require('./app/views/main.html')
+
+const App = new Vue({
+  el: '#app',
+  data() {
+    return {
+      name: null
+    }
+  },
+  methods: {
+    load() {
+      const file = dialog.showOpenDialog({
+        properties: ['openFile']
+      })
+
+      const audio = document.getElementById('test')
+
+      audio.src = file[0]
+      this.name = file[0]
+      audio.volume = 0.2
+      audio.autoplay = true
+    }
+  }
+})
